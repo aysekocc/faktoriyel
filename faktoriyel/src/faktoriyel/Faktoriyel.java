fdbcf604b82c2115afe7eed814be8d3b72f8d3e7
Ayşe Koç <ayseekoc481@gmail.com>
2023-01-19 21:28:45 +0300

    faktoriyel

---
+++ faktoriyel/src/faktoriyel/Faktoriyel.java
@@ -0,0 +1,20 @@
+﻿
+package faktoriyel;
+
+import java.util.Scanner;
+
+public class Faktoriyel {
+
+    public static void main(String[] args) {
+        Scanner k=new Scanner (System.in);
+        System.out.println("sayı gir:");
+        int sayi =k.nextInt();
+      int fak=1;
+        for (int i = 1; i <= sayi; i++) {
+            fak=fak*i;
+        }
+        System.out.println("SONUÇ:"+fak);
+}
+}
+    
+
